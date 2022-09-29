ab8949ba6065a79c5dc3e7f2fea0ef8f0990a905
hishamAbuodeh <H.Ibraheem@abuodeh.com>
2022-09-29 10:39:49 +0300

    edit

--- pages/home/index.js
+++ pages/home/index.js
@@ -100,7 +100,7 @@ export default function Home(props){
                 prevBillNo = billNo
                 setInnerLoading(true)
                 axios({
-                    baseURL:'http://https://alrayhan-rate.herokuapp.com/maltrans',
+                    baseURL:'https://alrayhan-rate.herokuapp.com/maltrans',
                     url: '/bill-of-lading',
                     method: 'post',
                     headers: {
